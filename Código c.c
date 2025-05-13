b8b993ea6cbccc0f5b91c12bbd886a18e4f52d33
gustav067 <gushdza23@gmail.com>
2025-05-13 09:37:13 -0600

    Código
    Gustavo Hernandez Apolinar

---
+++ Código c.c
@@ -0,0 +1,24 @@
+#include<stdio.h>
+
+
+ int *p,y;
+ 
+  void func()
+  {
+  int x = 40;
+  p=&x;
+  y=*p;
+  *p=23;
+  }
+  
+  int main (void)
+  {
+  func();
+  y=*p;
+  *p=25;
+  
+    printf("el valor de y es %d \n el valor de *p es %d \n el valor de p es %p", y, *p,p);
+    
+    return 0;
+ 
+ }
---
+++ Código referenciacion .c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+     int main (){
+     int x=17,y;
+     int * p;
+     P = &x;
+     printf("El valor de x es %d", *p);
+     Y=*p+3;
+     printf("El valor de y es %d", y);
+     
+     return 0;
+ 
+ }    
