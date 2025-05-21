a3575376da3dc96da6de63944aa00586d308a8e1
gustav067 <gushdza23@gmail.com>
2025-05-20 22:00:30 -0600

    Add files via upload
    20-05-2025

---
+++ Sizeof cadena.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+int main (){
+    
+    char cadena [10];
+    printf("un int ocupa %d bytes", sizeof(int));
+    printf("un char ocupa %d bytes", sizeof (char));
+    printf("un float ocupa %d bytes", sizeof (float));
+    printf("un double ocupa %d bytes", sizeof (double));
+    printf("cadena ocupa %d bytes", sizeof (cadena));
+    
+    return 0;
+      }
+
+
+
